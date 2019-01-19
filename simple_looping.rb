44ddcdf84b097d6b19ffe1bc426d0ae9f7c720d8
Sahir Nambiar <sahirnambiar@gmail.com>
2019-01-19 02:34:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,9 +36,13 @@ end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
+  # hint: use a counter to tell the until loop when to stop
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  counter = 0
+  until counter == number_of_times do
+    puts phrase
+    counter += 1
+  end
 end
 
 def for_iterator(number_of_times)
@@ -46,5 +50,7 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  for text in 1..number_of_times do
+    puts phrase
+  end
 end
